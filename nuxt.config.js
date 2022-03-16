9f95e1a0cebcde3b3e347def62c5eceff642708c
anonymous <anuwong@dollarsign.co>
2022-03-16 16:55:00 +0700

    fix: disables auto-import features

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
 </template>
 
 <script>
+import Tutorial from '@/components/Tutorial'
 export default {
   name: 'IndexPage',
+  components: { Tutorial },
 }
 </script>
