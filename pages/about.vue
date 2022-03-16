c415b7950a161c09f535e162f6a38eb89a352f9a
anonymous <anuwong@dollarsign.co>
2022-03-16 15:58:34 +0700

    🎉 initial commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <h1 class="font-bold text-3xl text-red-700 bg-red-100 rounded-xl block p-2">
+      About
+    </h1>
+    <p>this is about page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
